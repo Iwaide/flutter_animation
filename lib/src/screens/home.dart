86e17b8a6e03922706454e8e5df6b98ca564549f
Iwaide <iwaide@hoobby.net>
2020-01-26 14:29:57 +0900

    Add initState

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -6,12 +6,20 @@ class Home extends StatefulWidget {
   State<StatefulWidget> createState() => HomeState();
 }
 
-class HomeState extends State<Home> {
+class HomeState extends State<Home> with TickerProviderStateMixin {
   Animation<double> catAnimation;
   AnimationController catController;
 
   initState() {
     super.initState();
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(CurvedAnimation(
+      parent: catController,
+      curve: Curves.easeIn,
+    ));
   }
 
   Widget build(context) {
